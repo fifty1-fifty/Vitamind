acccb2b5d90369928f98792adbe319a10f25699b
fifty1-fifty <carterharman9@gmail.com>
2025-07-02 17:12:07 -0400

    Update TestPage.tsx

--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -1,12 +1,16 @@
 import Barcode from '../components/barcode-comp/barcode-scanner';
 import '../components/home-comp/home.css';
 
-
+const TestPage = () => 
+{
 
 
 
 
    return (  
         <div className='container-fluid' id='top-of-page'>
-          <Barcode
-        </div>
+          <Barcode />
+        </div>     
+);
+};
+export default TestPage;
