7641699889a3820f991ab9dfe9950c42839df587
fifty1-fifty <carterharman9@gmail.com>
2025-07-01 21:45:48 -0400

    Update ProductPage.tsx

--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -3,8 +3,8 @@ import { useLocation } from "react-router-dom";
 import Lookup from "../components/home-comp/lookup";
 import Background from '../components/background-comp/BackgroundMain.tsx';
 import Barcode from '../components/barcode-comp/barcode-scanner.tsx';
-import {buildPath } from '../../../utils.ts';
-import ProductStats from '../components/nutrients-comp/stat-layout.tsx';*/
+import {buildPath } from '../../../utils.ts';*/
+import ProductStats from '../components/nutrients-comp/stat-layout.tsx';
 
 
 
