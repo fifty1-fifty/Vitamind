8f92b07145bc906624aa06fa8a9fa0ef1e2d62a6
fifty1-fifty <carterharman9@gmail.com>
2025-07-01 21:50:11 -0400

    Update barcode-scanner.tsx

--- frontend/src/components/barcode-comp/barcode-scanner.tsx
+++ frontend/src/components/barcode-comp/barcode-scanner.tsx
@@ -21,7 +21,7 @@ const BarcodeScanner: React.FC<Props> = ({ switchOffScanner }) =>
 
 
 
-  const videoRef = useRef(any);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
   const [result, setResult] = useState('');
 
   useEffect(() => {
@@ -50,7 +50,7 @@ const BarcodeScanner: React.FC<Props> = ({ switchOffScanner }) =>
 
         <div className="live-feed-container">
           <video id="feed-styling" ref={videoRef? videoRef:undefined} />
-          {result && returnLog(result)}
+          {returnLog(result)}
           <button onClick={switchOffScanner}>Return</button>
           </div>
 
