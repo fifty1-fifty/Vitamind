6f9e0ad5504bc5da6de648eb52db906f1adc0778
fifty1-fifty <carterharman9@gmail.com>
2025-07-02 17:10:54 -0400

    Create TestPage.tsx

---
+++ frontend/src/pages/TestPage.tsx
@@ -0,0 +1,12 @@
+import Barcode from '../components/barcode-comp/barcode-scanner';
+import '../components/home-comp/home.css';
+
+
+
+
+
+
+   return (  
+        <div className='container-fluid' id='top-of-page'>
+          <Barcode
+        </div>
