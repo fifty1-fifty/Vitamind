ac09b21b571e7a299ea01322889a9990a1148eaf
fifty1-fifty <carterharman9@gmail.com>
2025-07-02 17:21:17 -0400

    Update TestPage.tsx

--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -1,15 +1,16 @@
+import {useState} from "react";
 import Barcode from '../components/barcode-comp/barcode-scanner';
 import '../components/home-comp/home.css';
 
 const TestPage = () => 
 {
-
+   //const [testNuts, setTestNuts] = useState('');
 
 
 
    return (  
         <div className='container-fluid' id='top-of-page'>
-          <Barcode switchOffScanner={(console.log('boobs'))} />
+          <Barcode switchOffScanner={() => console.log('boobs')} />
         </div>     
 );
 };
