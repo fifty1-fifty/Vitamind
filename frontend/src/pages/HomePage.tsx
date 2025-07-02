45a81b2298e385e4bc24e37412f3413ca9e8c08e
fifty1-fifty <carterharman9@gmail.com>
2025-07-02 11:46:03 -0400

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+/*import {useState} from "react";
 import Lookup from "../components/home-comp/lookup";
 //import Maintracker from "../components/home-comp/maintrack";
 import Background from '../components/background-comp/BackgroundMain.tsx';
@@ -6,7 +6,7 @@ import Barcode from '../components/barcode-comp/barcode-scanner.tsx';
 import DailyStats from '../components/home-comp/dailytrackedcircle.tsx';
 import DailyBar from '../components/home-comp/dailytrackedbar.tsx';
 import '../components/home-comp/home.css';
-
+*/
 
 
 const HomePage = () =>
