25ae76635471287089d1d5834b63e44ac14b34db
fifty1-fifty <carterharman9@gmail.com>
2025-08-28 08:16:18 -0400

    Add files via upload

--- frontend/src/components/TESTTESTTEST/log-scroll.tsx
+++ frontend/src/components/TESTTESTTEST/log-scroll.tsx
@@ -46,40 +46,47 @@ const DailyLog: React.FC<DailyLogProps> = ({ userDailyLog }) => {
     }
 
 
-    return (
-      <div
-        className="container-border"
-        style={{ width: '85%', marginTop: '3vh'}}
-      >
-        <div className="scroll-log-container" style={{justifyContent : userDailyLog.length === 0 ? 'center' : 'flex-start'}}>
-          {userDailyLog.length > 0 ? (
-            userDailyLog.map((item) => (
-              <div className="item-container" key={item.barcode}>
-                <div className="thumbnail-container" /* {style={{width : toggleHoverOverItemEffect ? '65%' : '60%'}} */>
-                  <img
-                    id="image-thumbnail"
-                    src={item.imageThumb}
-                    alt={item.productName}
-                    onClick={() => gotoProduct(item.barcode)}
-                    onTouchStart={(e) => handleTouchStart(e, item.barcode)}
-                    onTouchEnd={handleTouchEnd}
-                  />
-                  <span id="item-calories">{item.calories} cal</span>
-                </div>
-                <div className="full-text-container">
-                    <span id="item-title">{item.brandName !== "N/A" ? item.brandName : " "}</span>
-                    <span id="item-subtitle">{item.productName}</span>
-                </div>
-              </div>
-            ))
-          ) : (
-            <div className="placeholder-container text-center p-3">
-              <p className="placeholder-text">Your daily log is empty</p>
-            </div>
-          )}
+return (
+  <div className="container-border" style={{ width: '85%', marginTop: '3vh' }}>
+
+    <div className="scroll-log-container">
+
+      {userDailyLog.length > 0 && (
+        <div className="user-log-text-area">
+          <span className='user-log-text'>Daily Log</span>
         </div>
-      </div>
-    );
-};
+      )}
+
+      {userDailyLog.length > 0 ? (
+        userDailyLog.map((item) => (
+          <div className="item-container" key={item.barcode}>
+            <div className="thumbnail-container">
+              <img
+                id="image-thumbnail"
+                src={item.imageThumb}
+                alt={item.productName}
+                onClick={() => gotoProduct(item.barcode)}
+                onTouchStart={(e) => handleTouchStart(e, item.barcode)}
+                onTouchEnd={handleTouchEnd}
+              />
+              <span id="item-calories">{item.calories} cal</span>
+            </div>
+            <div className="full-text-container">
+              <span id="item-title">
+                {item.brandName !== "N/A" ? item.brandName : " "}
+              </span>
+              <span id="item-subtitle">{item.productName}</span>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div className="placeholder-container text-center p-3">
+          <p className="placeholder-text">Your daily log is empty</p>
+        </div>
+      )}
+    </div>
+  </div>
+);
+}
 
 export default DailyLog;
